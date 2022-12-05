3af4256d597fdb74a734a8b0d88eba1a004be18f
Jinmiao.Xie <xjmsvip@qq.com>
2022-12-05 11:44:16 +0800

    feat:project init

---
+++ src/routes/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const routes = [
+    {
+        path: '/',
+        name: 'Main',
+        loader: () => import('../main')
+    }
+];
+
+const createRoutes = (routes: any[]) =>
+    routes.map(({ loader, ...config }) => {
+        const Trunk = React.lazy(() => loader())
+        return {
+            ...config,
+            component: (
+                <React.Suspense>
+                    {/* fallback={<Loading />} */}
+                    <Trunk />
+                </React.Suspense>
+            ),
+        }
+    })
+
+const RoutesPage = () => {
+
+
+    return (
+        <Routes>
+            {createRoutes(routes).map((r) => {
+                // if (r.signin)
+                //     return (<Route key={r.path} path={r.path} element={
+                //         <RequirAuthRoute element={r.component} pathname={r.path}></RequirAuthRoute>
+                //     } />)
+                return <Route key={r.path} path={r.path} element={r.component} />
+            })}
+        </Routes>
+    )
+}
+
+
+export default RoutesPage
