0d82284677ef02d8824376245fae9b4d6a3a2db4
Jinmiao.Xie <xjmsvip@qq.com>
2022-12-12 15:23:41 +0800

    feat:add loge

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -34,20 +34,20 @@ export default () => {
         login.emit('sendWallet', { address: walletAddress, provider: provider, signer: curSigner });
 
         //
-        let signText = "Login-upgrade-doge-"+Date.parse(new Date().toString());
+        let signText = "Login-upgrade-doge-" + Date.parse(new Date().toString());
         let signContent = await curSigner.signMessage(signText);
         console.log("Sign content :", signContent);
 
         const loginRes = await request('login', {
-          method: 'post',
-          params: {
-            "address": walletAddress,
-            "signData": signText,
-            "signature": signContent
-          },
+            method: 'post',
+            params: {
+                "address": walletAddress,
+                "signData": signText,
+                "signature": signContent
+            },
         });
-        console.log("Login res :" ,loginRes);
-        if(loginRes.code == 0) {
+        console.log("Login res :", loginRes);
+        if (loginRes.code == 0) {
             login.emit('isLogin', true);
         }
 
@@ -70,14 +70,14 @@ export default () => {
         <div id="header">
             <div className="container">
                 <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-                    <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                        <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap" /></svg>
+                    <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none" style={{ fontSize: '2em' }}>
+                        🐶 Uprade Doge
                     </a>
                     <ul className="nav  col-md-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="/" className="nav-link px-2 link-dark">🐶Parser</a></li>
+                        <li><a href="/" className="nav-link px-2 link-dark">Parser</a></li>
                         <li><a href="/notifier" className="nav-link px-2 link-dark">Notifier</a></li>
                     </ul>
-                    <div className="col-md-5 text-end">
+                    <div className="col-md-3 text-end">
                         {isConnected ?
                             <>
                                 <span>{addressFormat} </span>
