02f39b7427b59fcb9acd6b7045cc483fc46b3fb8
metanomad21 <meta_nomad@126.com>
2022-12-12 14:43:09 +0800

    backspace format

--- src/common/common.ts
+++ src/common/common.ts
@@ -5,9 +5,9 @@ interface Common{
 export const CommonFun:Common = {
     ellipsis : (value: any, beforeCount = 4, afterCount = 4) => {
         return (
-            value.substr(0, beforeCount) +
-            "..." +
-            value.substr(value.length - afterCount)
-        );
+            value.substr(0, beforeCount) +
+            "..." +
+            value.substr(value.length - afterCount)
+        );
     }
 }
